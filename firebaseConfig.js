0d557d4b7e59ee662fec7aead47f438dfea0632c
dove209 <53247748+dove209@users.noreply.github.com>
2022-09-19 23:45:35 +0900

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
-// Initialize Firebase
-const app = initializeApp ({
+// Your web app's Firebase configuration
+const firebaseConfig = {
     apiKey: "AIzaSyB82f2xJGzRBBQ0cO7bllJoz11hVMDdqVE",
     authDomain: "gini-bnb-imageupload.firebaseapp.com",
     projectId: "gini-bnb-imageupload",
     storageBucket: "gini-bnb-imageupload.appspot.com",
     messagingSenderId: "314998679761",
     appId: "1:314998679761:web:497c8347f0e0104680bdf0"
-});
+  };
+  
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
  
 // Firebase storage reference
 const storage = getStorage(app);
