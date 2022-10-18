52b34676af488a71b717ddd0719f528e2139e340
GINI <dove209@naver.com>
2022-10-18 11:41:45 +0900

    Update index.ts

--- queryCache/index.ts
+++ queryCache/index.ts
@@ -1,9 +1,9 @@
 import { GetRoomListAPIQueries } from "../types/room";
 
 const queryCache = {
-    allRooms: (type: 'list' | 'map') => ['allRooms', type],
-    rooms: (queries: GetRoomListAPIQueries) => ['rooms', queries],
-    room: (roomId: string) => ['room', roomId],
+    allRooms: (type: 'list' | 'map') => ['allRooms', type] as const,
+    rooms: (queries: GetRoomListAPIQueries) => ['rooms', queries] as const,
+    room: (roomId: string) => ['room', roomId] as const,
 }
 
 export default queryCache;
